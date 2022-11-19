0215532323de4e0a853807dc5e2d83b7847f1efb
OctDeKos <ymdyadin@gmail.com>
2022-11-19 17:48:53 +0300

    Перегрузка.

--- src/main/java/Paket/Human.java
+++ src/main/java/Paket/Human.java
@@ -3,4 +3,14 @@ import org.example.App;
 //импорт пакета. Сначала название пакета, потом наименование класса.
 
 public class Human {
+
+
+    public Human() {
+    }
+    public Human(int q) {
+        System.out.println(q);
+    }
+    public Human(String q) {
+        System.out.println(q);
+    }
 }
--- src/main/java/Paket/Start.java
+++ src/main/java/Paket/Start.java
@@ -1,4 +1,8 @@
 package Paket;
 
 public class Start {
+    public static void main (String[] args) {
+        Human alena = new Human("Жирок");
+        //пример перегрузки, когда в классе Human создано несколько методов.
+    }
 }
